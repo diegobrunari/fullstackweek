4a42ec549ae4422f538feb49f1929ac6e27a4704
Diego Brunari <diego.skp@hotmail.com>
2024-05-01 15:17:37 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -144,6 +144,10 @@ const ProductDetails = ({
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar a sacola</Button>
+      </div>
     </div>
   );
 };
