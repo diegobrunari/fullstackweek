b49e5998746074f74f76828532f7d6b6c5378c66
Diego Brunari <diego.skp@hotmail.com>
2024-05-03 01:47:11 -0300

    feat: ensure user can redo order

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -96,9 +96,12 @@ const Header = () => {
               <Button
                 variant="ghost"
                 className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+                asChild
               >
-                <HomeIcon size={16} />
-                <span className="block">Início</span>
+                <Link href="/">
+                  <HomeIcon size={16} />
+                  <span className="block">Início</span>
+                </Link>
               </Button>
               {data?.user && (
                 <>
